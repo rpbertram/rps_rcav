2ceb4114eac45fd03a183278de11ef1d50ea72b9
Ryan Bertram <rpbertram@users.noreply.github.com>
2016-10-24 12:39:45 -0500

    Completed tasks

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,10 +1,21 @@
 class GameController < ApplicationController
 # < this allows us to inherit the ApplicationController stuff
  def user_plays_rock
-
    moves = ["rock", "paper", "scissors"]
    @computer_move = moves.sample
    render("game/play_rock.html.erb")
    #this filename
+ end
+
+ def user_plays_paper
+   moves = ["rock", "paper", "scissors"]
+   @computer_move = moves.sample
+   render("game/play_paper.html.erb")
+ end
+
+ def user_plays_scissors
+   moves = ["rock", "paper", "scissors"]
+   @computer_move = moves.sample
+   render("game/play_scissors.html.erb")
  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
 
+  root :to => redirect("/rock")
   get("/rock", { :controller => "game", :action => "user_plays_rock"})
-  
+  get("/paper", { :controller => "game", :action => "user_plays_paper"})
+  get("/scissors", { :controller => "game", :action => "user_plays_scissors"})
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
